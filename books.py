1efbcfc132062599c6c0d62321968115e241eed8
yusufaliselek <yusufaliselek41@gmail.com>
2022-09-01 12:28:19 +0300

    create book func

---
+++ books.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from enum import Enum
+app = FastAPI()
+
+BOOKS = {
+    "book_1": {"title": "Title One", "author": "Author one"},
+    "book_2": {"title": "Title Two", "author": "Author Two"},
+    "book_3": {"title": "Title Three", "author": "Author Three"},
+    "book_4": {"title": "Title Four", "author": "Author Four"},
+    "book_5": {"title": "Title Five", "author": "Author Five"},
+}
+
+class DirectionName(str, Enum):
+    north = "North"
+    south = "South"
+    east = "East"
+    west = "West"
+
+
+@app.get("/")
+async def read_all_books():
+    return BOOKS
+
+@app.get("/{book_name}")
+async def read_book(book_name: str):
+    return BOOKS[book_name]
+
+#@app.get("/directions/{direction_name}")
+#async def get_direction(direction_name: DirectionName):
+#    if direction_name == DirectionName.north:
+#       return {"Direction": direction_name, "sub": "Up"}
+#    if direction_name == DirectionName.south:
+#       return {"Direction": direction_name, "sub": "Down"}
+#    if direction_name == DirectionName.west:
+#       return {"Direction": direction_name, "sub": "Left"}
+#return {"Direction": direction_name, "sub": "Right"}
